83bf7487f61c0ca2ba2e92b06e631c91f9f5af1c
Simas <simasleoleonavicius@gmail.com>
2019-08-25 13:44:57 +0300

    Complete Lesson 5 - Event Handling of Intro to Vue.js course.

---
+++ Intro to Vue.js/5. Event Handling/main.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+   el: '#app',
+   data: {
+       product: 'Socks',
+       image: '../../assets/images/vmSocks-green-onWhite.jpg',
+       inventory: 100,
+       details: ["80% cotton", "20% polyester", "Gender-neutral"],
+       variants: [
+           {
+               variantId: 2234,
+               variantColor: "green",
+               variantImage: '../../assets/images/vmSocks-green-onWhite.jpg',
+           },
+           {
+               variantId: 2235,
+               variantColor: "blue",
+               variantImage: '../../assets/images/vmSocks-blue-onWhite.jpg',
+           },
+       ],
+       cart: 0,
+   },
+    methods: {
+       addToCart: function () {
+           this.cart += 1;
+       },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
+    }
+});
