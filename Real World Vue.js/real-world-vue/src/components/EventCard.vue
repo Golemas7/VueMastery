be93abdbb7cd7985b19edb74d7ec206844c8d35d
Simas Leonavičius <simasleoleonavicius@gmail.com>
2019-09-01 23:26:27 +0300

    Complete Lesson 9 - API calls with Axios for Real World Vue.js course.

--- Real World Vue.js/real-world-vue/src/components/EventCard.vue
+++ Real World Vue.js/real-world-vue/src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     class="event-link"
-    :to="{ name: 'event-show', params: { id: '1' } }"
+    :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
       <span>@{{ event.time }} on {{ event.date }}</span>
@@ -14,28 +14,11 @@
 </template>
 
 <script>
-import BaseIcon from "./BaseIcon";
+import BaseIcon from './BaseIcon'
 export default {
-  components: {BaseIcon},
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Beach Cleanup',
-        date: 'Tues Aug 19, 2018',
-        time: '6:00',
-        attendees: [
-          {
-            id: 'abc123',
-            name: 'Adam Jahr'
-          },
-          {
-            id: 'def456',
-            name: 'Gregg Pollack'
-          }
-        ]
-      }
-    }
+  components: { BaseIcon },
+  props: {
+    event: Object
   }
 }
 </script>
--- Real World Vue.js/real-world-vue/src/views/EventList.vue
+++ Real World Vue.js/real-world-vue/src/views/EventList.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <event-card />
+    <event-card v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard'
+import EventService from '@/services/EventService'
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error:' + error.response)
+      })
   }
 }
 </script>
